85f6d50fb787588669a41dcd89177fc8f3257be8
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2017-10-20 14:03:40 -0700

    Container v1.0.16

---
+++ lib/src/main/java/com/ern/api/impl/PetApiRequestHandlerProvider.java
@@ -0,0 +1,76 @@
+/*
+* Copyright 2017 WalmartLabs
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.ern.api.impl;
+
+import android.support.annotation.Nullable;
+
+/**
+* This class is a generated place holder for your Pet implementations.!
+* Feel free to modify this class contents as needed. `ern regen-api-impl` command WILL NOT modify the content of this class.
+* Don't modify the class name as this naming convention is used for container generation.
+*/
+public class PetApiRequestHandlerProvider extends RequestHandlerProvider<PetApiRequestHandlerProvider.PetApiConfig> implements PetApiRequestHandler {
+
+    /**
+    * @param requestHandlerConfig : Optional config object that can be passed to an api impl provider.
+    */
+    PetApiRequestHandlerProvider(@Nullable PetApiRequestHandlerProvider.PetApiConfig requestHandlerConfig) {
+        super(requestHandlerConfig);
+    }
+
+    @Override
+    public void registerUpdatePetRequestHandler() {
+        //TODO
+    }
+
+    @Override
+    public void registerAddPetRequestHandler() {
+        //TODO
+    }
+
+    @Override
+    public void registerFindPetsByStatusRequestHandler() {
+        //TODO
+    }
+
+    @Override
+    public void registerFindPetsByTagsRequestHandler() {
+        //TODO
+    }
+
+    @Override
+    public void registerGetPetByIdRequestHandler() {
+        //TODO
+    }
+
+    @Override
+    public void registerUpdatePetWithFormRequestHandler() {
+        //TODO
+    }
+
+    @Override
+    public void registerDeletePetRequestHandler() {
+        //TODO
+    }
+
+    @Override
+    public void registerUploadFileRequestHandler() {
+        //TODO
+    }
+
+    // DO NOT rename this class as this naming convention is used when a container is generated.
+    public static class PetApiConfig implements RequestHandlerConfig {
+
+    }
+}
